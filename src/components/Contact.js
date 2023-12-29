a6a5066c152e3c550297612a0017379cc3905791
dhirajbantawarai <61051104+dhirajbantawarai@users.noreply.github.com>
2023-12-29 13:21:12 +0545

    contact pge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import "../styles/contact.css";
 export const Contact = () => {
   return (
-    <h1>Contact</h1>
-  )
-}
+    <>
+      <div className="contact-page">
+        <h1>Contact Us</h1>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+
+            <input type="text" id="name" name="name" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-control"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+
+            <textarea
+              id="message"
+              name="message"
+              rows="5"
+              className="form-control"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
--- src/components/Signup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { L } from 'react-router-dom';
-
-
-export const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
